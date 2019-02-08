05e53c962f31ec92996441adbc57457c77a23d0d
Bishal Prasad <biprasad@microsoft.com>
2019-02-08 16:07:46 +0530

    Updated VSS.SDK and content

--- sample/Scripts/ResourceSection.js
+++ sample/Scripts/ResourceSection.js
@@ -2,38 +2,43 @@
     "use strict";
 
     VSS.ready(function() {
-        var initialConfig = VSS.getConfiguration();
-        var text = "Resource id passed as initial config: " + initialConfig.resourceId;
-        console.log(text);
-        document.getElementsByClassName("fxs-frame-content")[0].innerText = text;
-        initialConfig.getResourceData(initialConfig.resourceId, "2018-04-01").then(function (result) {
-            document.getElementsByClassName("fxs-frame-content2")[0].innerText = "Resource name: " + result.name;  
-        });
-
-        VSS.getAccessToken().then(function (result) {
-            document.getElementsByClassName("fxs-frame-token")[0].innerText = result.token;
-            $.ajax
-            ({
-              type: "GET",
-              url: "https://bishaldft1.vsrm.vsts.me/_apis/release/releases",
-              dataType: 'json',
-              async: false,
-              beforeSend: function (xhr) {
-                xhr.setRequestHeader ("Authorization", "Bearer " + result.token);
-              },
-              success: function (value){
-                console.log("count = " + value.length)
-              }
-          });
-        });
-
+        
+        main();
         VSS.register("resource-section", {
-            setResourceId: function (resourceId) {
-                var text2 = "Resource id called and set by parent: " + resourceId;
-                console.log(text2);
-                document.getElementsByClassName("fxs-frame-content2")[0].innerText = text2;    
+            refresh: function () {
+                main(); 
             }
         });
         VSS.notifyLoadSucceeded();
     });
+
+    function main() {
+        var initialConfig = VSS.getConfiguration();
+        var text = "Azure resource Uri: " + initialConfig.resourceId;
+        console.log(text);
+        document.getElementsByClassName("fxs-frame-resource-uri")[0].innerText = text;
+        initialConfig.getResourceData(initialConfig.resourceId, "2018-04-01").then(function (result) {
+            document.getElementsByClassName("fxs-frame-resource-name")[0].innerText = "Resource name: " + result.name;
+        });
+        
+        VSS.getAccessToken().then(function (result) {
+            
+            $.ajax
+            ({
+              method: "GET",
+              url: `https://vsrm.dev.azure.com/${initialConfig.orgName}/${initialConfig.projectId}/_apis/release/releases?definitionId=${initialConfig.releaseDefinitionId}&$top=1&$expand=environments`,
+              dataType: 'json',
+              crossDomain: true,
+              contentType: "application/json",
+              async: true,
+              cache:false,
+              beforeSend: function (xhr) {
+                xhr.setRequestHeader ("Authorization", "Bearer " + result.token);
+              },
+              success: function (value){
+                document.getElementsByClassName("fxs-frame-status")[0].innerText = "Deployment status: " + value.value[0].environments[0].status;
+              }
+          });
+        });
+    }
 })();
